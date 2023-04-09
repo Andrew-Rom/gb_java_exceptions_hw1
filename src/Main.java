9fb1bb8177c83aa7d4fe27372cccfc7fb29a38a0
Andrew-Rom <1811mail@gmail.com>
2023-04-09 11:31:33 +0300

    Completed task1

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,32 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        /**
-         * HW1. Task1
-         * Реализуйте метод checkArray(Integer[] arr), принимающий в качестве аргумента целочисленный одномерный массив.
-         * Метод должен пройтись по каждому элементу и проверить что он не равен null.
-         * А теперь реализуйте следующую логику:
-         * Если в какой-то ячейке встретился null, то необходимо “оповестить” об этом пользователя
-         * Если null’ы встретились в нескольких ячейках, то идеально было бы все их “подсветить”
+        /*
+          HW1. Task1
+          Реализуйте метод checkArray(Integer[] arr), принимающий в качестве аргумента целочисленный одномерный массив.
+          Метод должен пройтись по каждому элементу и проверить что он не равен null.
+          А теперь реализуйте следующую логику:
+          Если в какой-то ячейке встретился null, то необходимо “оповестить” об этом пользователя
+          Если null’ы встретились в нескольких ячейках, то идеально было бы все их “подсветить”
          */
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an array of integers > ");
-        String line = scanner.nextLine();
-        scanner.close();
-
-        Scanner scanner1 = new Scanner(line);
-        ArrayList<Integer> digitsFromLine = new ArrayList<>();
-        while (scanner1.hasNextInt()) {
-            digitsFromLine.add(scanner1.nextInt());
-        }
-        scanner.close();
-
-        int[] array = new int[digitsFromLine.size()];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = digitsFromLine.get(i);
-        }
-
-        System.out.println(Arrays.toString(array));
+        Integer[] array = {1, 10, null, 5, 4, null, 9};
+        checkArray(array);
 
     }
 
-    public static void checkArray(int[]checkArr) {
-        for (int i = 0; i < ; i++) {
+    public static void checkArray(Integer[] checkArr) {
+        ArrayList<Integer> listOfNullIndexes = new ArrayList<>();
+        for (int i = 0; i < checkArr.length; i++) {
             if (checkArr[i] == null) {
-                throw
+                listOfNullIndexes.add(i);
             }
-
+        }
+        if (listOfNullIndexes.size() == 0) {
+            System.out.println("Array does not contain null item");
+        } else {
+            System.out.println("Array contains null item with index: " + listOfNullIndexes);
         }
     }
 
